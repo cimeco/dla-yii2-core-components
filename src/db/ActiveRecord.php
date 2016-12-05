d16a8261a25fe270f53d2a93c3bc2da9be13ad3f
Carlos A. Garcia <garciac12@gmail.com>
2016-12-05 15:13:45 -0300

    Se agrega la auto carga de behaviors.

--- src/db/ActiveRecord.php
+++ src/db/ActiveRecord.php
@@ -8,7 +8,28 @@ namespace quoma\core\db;
  * @author mmoyano
  */
 class ActiveRecord extends \yii\db\ActiveRecord{
-    
+
+    /**
+     * Carga dinamicamente los behaviors en base a los configurados en params.
+     * Para poder cargarlos en params tiene que tener un parametro de tipo array con la siguiente estructura:
+     *
+     * 'ClaseBehaviors' => [
+     *          'namespace\\clase\\behavior'
+     * ]
+     *
+     * @return array
+     */
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        if( array_key_exists(get_class($this).'Behaviors', \Yii::$app->params)) {
+            $behaviors = array_merge($behaviors, \Yii::$app->params[get_class($this).'Behaviors']);
+        }
+
+        return $behaviors;
+    }
+
     public function getDeletable(){
         
         return false;
