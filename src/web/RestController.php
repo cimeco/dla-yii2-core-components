5e159f0c6f16b006ff69e0b293b629b11bff680d
Carlos A. Garcia <garciac12@gmail.com>
2017-03-23 10:43:31 -0300

    Se agrega login y behavior de login

--- src/web/RestController.php
+++ src/web/RestController.php
@@ -9,9 +9,13 @@
 namespace quoma\core\web;
 
 use yii\filters\auth\HttpBasicAuth;
+use yii\web\Response;
 
 /**
  * REST Authentication
+ * Controlador a heredar para el uso de REST.
+ * Ya incorpora la seguridad via behavior, en caso de que no se requiera seguridad,
+ * hay que colocar el parametro auth_api = false en el params.
  *
  * @author martin
  */
@@ -19,24 +23,69 @@ class RestController extends \yii\rest\ActiveController{
 
     public function behaviors()
     {
+        $auth_api = (isset(\Yii::$app->params['auth_api']) ? \Yii::$app->params['auth_api'] : true );
+
         $behaviors = parent::behaviors();
-        $behaviors['authenticator'] = [
-            'except' => ['login'],
-            'class' => HttpBasicAuth::className(),
-            'auth' => function ($username, $password) {
-                // Return Identity object or null
-                $user = \webvimark\modules\UserManagement\models\User::findOne([
-                    'username' => $username,
-                ]);
-
-                if($user && $user->validatePassword($password)){
-                    return $user;
-                }
-
-                return null;
-            },
-
+        $behaviors =  [
+            [
+                'class' => 'yii\filters\ContentNegotiator',
+                'only' => ['view', 'index', 'login'],  // in a controller
+                // if in a module, use the following IDs for user actions
+                // 'only' => ['user/view', 'user/index']
+                'formats' => [
+                    'application/json' => Response::FORMAT_JSON,
+                    'application/xml' => Response::FORMAT_XML,
+                ],
+            ],
         ];
+
+        if($auth_api) {
+            $behaviors['authenticator'] = [
+                'except' => ['login'],
+                'class' => HttpBasicAuth::className(),
+                'auth' => function ($username, $password) {
+                    // Return Identity object or null
+                    $user = \webvimark\modules\UserManagement\models\User::findOne([
+                        'username' => $username,
+                    ]);
+
+                    if($user && $user->validatePassword($password)){
+                        return $user;
+                    }
+
+                    return null;
+                },
+            ];
+        }
+
         return $behaviors;
+    }
+
+
+    /**
+     * Login para obtener auth_key para utilizar la api
+     */
+    public function actionLogin()
+    {
+
+        $model = new \webvimark\modules\UserManagement\models\forms\LoginForm();
+        $model->username = Yii::$app->request->post('username');
+        $model->password = Yii::$app->request->post('password');
+
+        $model->validate();
+
+        if ( !$model->hasErrors() )
+        {
+            //$identity = Yii::$app->user->identity;
+
+            return [
+                'status' => 'success',
+                'auth_key' => $model->getUser()->auth_key,
+                'username' => $model->getUser()->username
+            ];
+        }
+
+        throw new \yii\web\HttpException(401, 'Not authorized.');
+
     }
 }
