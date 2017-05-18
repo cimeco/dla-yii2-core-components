e6936d5f3bfbcece3ac53d4cbf2f89d36c9a3113
Marcelo Briones <ing@marcelobriones.com.ar>
2017-05-18 17:51:52 -0300

    Improves DbHelper

--- src/helpers/DbHelper.php
+++ src/helpers/DbHelper.php
@@ -7,7 +7,8 @@ namespace quoma\core\helpers;
  *
  * @author marcelo
  */
-class DbHelper {
+class DbHelper
+{
 
     public static function getDbName($db = 'db')
     {
@@ -21,7 +22,7 @@ class DbHelper {
 
     public static function getDbHost($db = 'db')
     {
-        $dsn = Yii::$app->get($db)->dsn;
+        $dsn = \Yii::$app->get($db)->dsn;
         if (preg_match('/' . 'host' . '=([^;]*)/', $dsn, $match)) {
             return $match[1];
         } else {
@@ -31,7 +32,7 @@ class DbHelper {
 
     public static function getDbPort($db = 'db')
     {
-        $dsn = Yii::$app->get($db)->dsn;
+        $dsn = \Yii::$app->get($db)->dsn;
         if (preg_match('/' . 'port' . '=([^;]*)/', $dsn, $match)) {
             return $match[1];
         } else {
@@ -41,12 +42,21 @@ class DbHelper {
 
     public static function getDbUsername($db = 'db')
     {
-        return Yii::$app->get($db)->username;
+        return \Yii::$app->get($db)->username;
     }
 
     public static function getDbPassword($db = 'db')
     {
-        return Yii::$app->get($db)->password;
+        return \Yii::$app->get($db)->password;
     }
 
+    public static function execute($sql, $db = 'db')
+    {
+        return \Yii::$app->get($db)->createCommand($sql)->execute();
+    }
+
+    public static function query($sql, $db = 'db')
+    {
+        return \Yii::$app->get($db)->createCommand($sql)->queryAll();
+    }
 }
